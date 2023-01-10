7077e07d515234bb9c7a3826b63f309765303542
Alexis Valentin <alexis.jean.valentin@gmail.com>
2023-01-10 21:51:02 +0100

    [CONFIG] update dependencies + get rid of warnings

--- src/pages/HallOfFame/HallOfFame.styled.jsx
+++ src/pages/HallOfFame/HallOfFame.styled.jsx
@@ -1,6 +1,3 @@
-import styled from "styled-components"
-import { Link } from "react-router-dom"
+import styled from "styled-components";
 
-export const HallOfFameContainer = styled.div`
-    
-`
+export const HallOfFameContainer = styled.div``;
